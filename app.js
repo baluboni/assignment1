59ea47f5125054c62bd46e678d4643d03d1815fd
baluboni <148212227+baluboni@users.noreply.github.com>
2023-11-30 10:46:14 +0530

    app.js

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ let hd=React.createElement("h1",{},"This is from react")
 let el=document.getElementById("root")
 let ad=ReactDOM.createRoot(el)
 ad.render(hd)
+//nested react element
+let nest=React.createElement("h1",{},"Another heading ")
+let nested=React.createElement("paragraph",{},"Another paragraph ")
+let element=document.getElementById("root")
+let root1=ReactDOM.createRoot(element)
+root1.render(React.createElement("div",{},nest,nested));
